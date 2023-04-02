d677125924658ef945b0baa2910107340c4710a5
Giridhar kirthik H <89655168+giridharkirthik@users.noreply.github.com>
2023-04-02 19:39:45 +0530

    Update settings.py

--- dev/django-k8s/web/django_k8s/settings.py
+++ dev/django-k8s/web/django_k8s/settings.py
@@ -159,3 +159,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+
+]
+from .cdn.conf import * # noqa
